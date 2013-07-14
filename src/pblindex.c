70aa4babdc8ac1cfc707b244e1b914f5d535d7ae
Edward Patel <edward.patel@memention.com>
2013-07-14 21:04:40 +0200

    fix inbound buffer size for Android

--- src/pblindex.c
+++ src/pblindex.c
@@ -47,98 +47,98 @@ Window window;
 TextLayer textLayer[2][NUM_LINES];
 
 void set_display_fail(char *text) {
-	text_layer_set_text(&textLayer[0][0], "Failed");
-	text_layer_set_text(&textLayer[0][1], text);
-	for (int i=2; i<NUM_LINES; i++)
-		text_layer_set_text(&textLayer[0][i], "");
-	for (int i=0; i<NUM_LINES; i++)
-		text_layer_set_text(&textLayer[1][i], "");
+    text_layer_set_text(&textLayer[0][0], "Failed");
+    text_layer_set_text(&textLayer[0][1], text);
+    for (int i=2; i<NUM_LINES; i++)
+        text_layer_set_text(&textLayer[0][i], "");
+    for (int i=0; i<NUM_LINES; i++)
+        text_layer_set_text(&textLayer[1][i], "");
 }
 
 void request_names() {
-	DictionaryIterator *body;
+    DictionaryIterator *body;
 #error Set URL below
-	// http://nnnn/nnnn.names should return something like (not over 76 bytes)
-	// {"0":"OMXS30","1":"Dow Jones","2":"Nasdaq","3":"DAX","4":"Nikkei"}
-	if (http_out_get("http://nnnn/nnnn.names", false, PBLINDEX_NAMES_COOKIE, &body) != HTTP_OK ||
+    // http://nnnn/nnnn.names should return something like (not over 76 bytes)
+    // {"0":"OMXS30","1":"Dow Jones","2":"Nasdaq","3":"DAX","4":"Nikkei"}
+    if (http_out_get("http://nnnn/nnnn.names", false, PBLINDEX_NAMES_COOKIE, &body) != HTTP_OK ||
         http_out_send() != HTTP_OK) {
-		set_display_fail("names..");
-	}
+        set_display_fail("names..");
+    }
 }
 
 void request_values() {
-	DictionaryIterator *body;
+    DictionaryIterator *body;
 #error Set URL below
-	// http://nnnn/nnnn.values should return something like (not over 76 bytes)
-	// {"0":"1210","1":"15464","2":"3600","3":"8212","4":"14506"}
-	if (http_out_get("http://nnnn/nnnn.values", false, PBLINDEX_VALUES_COOKIE, &body) != HTTP_OK ||
-		http_out_send() != HTTP_OK) {
-		set_display_fail("values..");
-	}
+    // http://nnnn/nnnn.values should return something like (not over 76 bytes)
+    // {"0":"1210","1":"15464","2":"3600","3":"8212","4":"14506"}
+    if (http_out_get("http://nnnn/nnnn.values", false, PBLINDEX_VALUES_COOKIE, &body) != HTTP_OK ||
+        http_out_send() != HTTP_OK) {
+        set_display_fail("values..");
+    }
 }
 
 void failed(int32_t cookie, int http_status, void *ctx) {
-	if (cookie == 0 ||
-		cookie == PBLINDEX_NAMES_COOKIE ||
+    if (cookie == 0 ||
+        cookie == PBLINDEX_NAMES_COOKIE ||
         cookie == PBLINDEX_VALUES_COOKIE) {
-		set_display_fail("failed()");
-	}
+        set_display_fail("failed()");
+    }
 }
 
 void success(int32_t cookie, int http_status, DictionaryIterator *dict, void *ctx) {
-	if (cookie != PBLINDEX_NAMES_COOKIE && cookie != PBLINDEX_VALUES_COOKIE)
-		return;
-	int li = cookie == PBLINDEX_NAMES_COOKIE ? 0 : 1;
-	for (int i=0; i<NUM_LINES; i++) {
-		Tuple *value = dict_find(dict, i);
-		if (value) {
-			static char str[2][NUM_LINES][16];
-			strcpy(str[li][i], value->value->cstring);
-			text_layer_set_text(&textLayer[li][i], str[li][i]);
-		} else {
-			text_layer_set_text(&textLayer[li][i], "-");
-		}
-	}
-	if (!li)
-		request_values();
-	else
-		light_enable_interaction();
+    if (cookie != PBLINDEX_NAMES_COOKIE && cookie != PBLINDEX_VALUES_COOKIE)
+        return;
+    int li = cookie == PBLINDEX_NAMES_COOKIE ? 0 : 1;
+    for (int i=0; i<NUM_LINES; i++) {
+        Tuple *value = dict_find(dict, i);
+        if (value) {
+            static char str[2][NUM_LINES][16];
+            strcpy(str[li][i], value->value->cstring);
+            text_layer_set_text(&textLayer[li][i], str[li][i]);
+        } else {
+            text_layer_set_text(&textLayer[li][i], "-");
+        }
+    }
+    if (!li)
+        request_values();
+    else
+        light_enable_interaction();
 }
 
 void reconnect(void *ctx) {
-	request_names();
+    request_names();
 }
 
 void init_handler(AppContextRef ctx) {
-	window_init(&window, "pbl-index");
-	window_set_background_color(&window, GColorBlack);
-	window_stack_push(&window, true);
+    window_init(&window, "pbl-index");
+    window_set_background_color(&window, GColorBlack);
+    window_stack_push(&window, true);
     
-	for (int i=0; i<NUM_LINES; i++) {
-		text_layer_init(&textLayer[0][i], GRect(5, 7+i*30, 144-5-COLUMN2_WIDTH, 30));
-		text_layer_init(&textLayer[1][i], GRect(144-COLUMN2_WIDTH, 7+i*30, COLUMN2_WIDTH, 30));
-		text_layer_set_font(&textLayer[0][i], fonts_get_system_font(FONT_KEY_GOTHIC_24));
-		text_layer_set_font(&textLayer[1][i], fonts_get_system_font(FONT_KEY_GOTHIC_24));
-		text_layer_set_background_color(&textLayer[0][i], GColorBlack);
-		text_layer_set_background_color(&textLayer[1][i], GColorBlack);
-		text_layer_set_text_color(&textLayer[0][i], GColorWhite);
-		text_layer_set_text_color(&textLayer[1][i], GColorWhite);
-		text_layer_set_text_alignment(&textLayer[1][i], GTextAlignmentRight);
-	}
+    for (int i=0; i<NUM_LINES; i++) {
+        text_layer_init(&textLayer[0][i], GRect(5, 7+i*30, 144-5-COLUMN2_WIDTH, 30));
+        text_layer_init(&textLayer[1][i], GRect(144-COLUMN2_WIDTH, 7+i*30, COLUMN2_WIDTH, 30));
+        text_layer_set_font(&textLayer[0][i], fonts_get_system_font(FONT_KEY_GOTHIC_24));
+        text_layer_set_font(&textLayer[1][i], fonts_get_system_font(FONT_KEY_GOTHIC_24));
+        text_layer_set_background_color(&textLayer[0][i], GColorBlack);
+        text_layer_set_background_color(&textLayer[1][i], GColorBlack);
+        text_layer_set_text_color(&textLayer[0][i], GColorWhite);
+        text_layer_set_text_color(&textLayer[1][i], GColorWhite);
+        text_layer_set_text_alignment(&textLayer[1][i], GTextAlignmentRight);
+    }
     
-	text_layer_set_text(&textLayer[0][0], "pbl-index");
-	text_layer_set_text(&textLayer[0][1], "by epatel");
-	text_layer_set_text(&textLayer[0][3], "loading...");
+    text_layer_set_text(&textLayer[0][0], "pbl-index");
+    text_layer_set_text(&textLayer[0][1], "by epatel");
+    text_layer_set_text(&textLayer[0][3], "loading...");
     
-	for (int i=0; i<NUM_LINES; i++) {
-		layer_add_child(&window.layer, &textLayer[0][i].layer);
-		layer_add_child(&window.layer, &textLayer[1][i].layer);
-	}
+    for (int i=0; i<NUM_LINES; i++) {
+        layer_add_child(&window.layer, &textLayer[0][i].layer);
+        layer_add_child(&window.layer, &textLayer[1][i].layer);
+    }
     
-	http_set_app_id(PBLINDEX_NAMES_COOKIE);
+    http_set_app_id(PBLINDEX_NAMES_COOKIE);
     
-	http_register_callbacks((HTTPCallbacks){
-		.failure = failed,
+    http_register_callbacks((HTTPCallbacks){
+        .failure = failed,
         .success = success,
         .reconnect = reconnect,
     }, NULL);
@@ -151,7 +151,7 @@ void pbl_main(void *params) {
         .init_handler = &init_handler,
         .messaging_info = {
             .buffer_sizes = {
-                .inbound = 256,
+                .inbound = 124, // 124 safe for Android
                 .outbound = 256,
             }
         },
