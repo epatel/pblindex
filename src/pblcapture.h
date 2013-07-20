44236fac0ec620b9aa734d2a0e431b7ffa5b25c7
Edward Patel <edward.patel@memention.com>
2013-07-20 17:45:01 +0200

    license headers for pblcapture

--- src/pblcapture.h
+++ src/pblcapture.h
@@ -1,3 +1,26 @@
+/* ===========================================================================
+ 
+ Copyright (c) 2013 Edward Patel
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ 
+ =========================================================================== */
 
 #define PBL_CAPTURE_UUID { 0xAB, 0xC1, 0x37, 0x4F, 0xA9, 0x4B, 0x4F, 0xBB, 0x95, 0x31, 0x63, 0x47, 0x84, 0x2F, 0xCD, 0xB1 }
 
