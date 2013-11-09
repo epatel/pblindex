9818b724a8a598f6767263bb25242ab2ca63b169
Edward Patel <edward.patel@memention.com>
2013-11-09 02:03:08 +0100

    Updated to use the new JS bridge instead of httpebble

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,65 @@
+/* ===========================================================================
+ 
+ Copyright (c) 2013 Edward Patel
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ 
+ =========================================================================== */
+
+function fetchList(list, url) {
+  var response;
+  var req = new XMLHttpRequest();
+  req.open('GET', url, true);
+  req.onload = function(e) {
+    if (req.readyState == 4) {
+      if(req.status == 200) {
+        console.log(req.responseText);
+        response = JSON.parse(req.responseText);
+        if (response) {
+          response[list] = 1;
+          Pebble.sendAppMessage(response);
+        }
+      } else {
+        console.log("Request returned error code " + req.status.toString());
+      }
+    }
+  }
+  req.send(null);
+}
+
+Pebble.addEventListener("ready",
+                        function(e) {
+                          console.log("connect!" + e.ready);
+                          console.log(e.type);
+                        });
+
+Pebble.addEventListener("appmessage",
+                        function(e) {
+                          console.log("message");
+                          if (e.payload.names) {
+                            // http://nnnn/nnnn.names should return something like
+                            // {"0":"OMXS30","1":"Dow Jones","2":"Nasdaq","3":"DAX","4":"Nikkei"}
+                            fetchList("names", "http://nnnn/nnnn.names");
+                          }
+                          if (e.payload.values) {
+                            // http://nnnn/nnnn.values should return something like
+                            // {"0":"1210","1":"15464","2":"3600","3":"8212","4":"14506"}
+                            fetchList("values", "http://nnnn/nnnn.values");
+                          }
+                        });
--- src/pblindex.c
+++ src/pblindex.c
@@ -24,15 +24,13 @@
 
 #include "pebble.h"
 
-#include "http.h"
-#include "httpcapture.h"
-
 #define NUM_LINES 5
 #define COLUMN2_WIDTH 65
 
-#error Set values below, should be different, one unique for each request
-#define PBLINDEX_NAMES_COOKIE some-unique-value
-#define PBLINDEX_VALUES_COOKIE some-unique-value
+enum {
+    QUOTE_KEY_NAMES  = 10,
+    QUOTE_KEY_VALUES = 11,
+};
 
 Window *window;
 TextLayer *textLayer[2][NUM_LINES];
@@ -46,42 +44,34 @@ void set_display_fail(char *text) {
         text_layer_set_text(textLayer[1][i], "");
 }
 
-void request_names() {
-    DictionaryIterator *body;
-#error Set URL below
-    // http://nnnn/nnnn.names should return something like (not over 76 bytes)
-    // {"0":"OMXS30","1":"Dow Jones","2":"Nasdaq","3":"DAX","4":"Nikkei"}
-    if (http_out_get("http://nnnn/nnnn.names", false, PBLINDEX_NAMES_COOKIE, &body) != HTTP_OK ||
-        http_out_send() != HTTP_OK) {
-        set_display_fail("names..");
-    }
-}
+void request_list(int list) {
+    Tuplet list_tuple = TupletInteger(list, 1);
 
-void request_values() {
-    DictionaryIterator *body;
-#error Set URL below
-    // http://nnnn/nnnn.values should return something like (not over 76 bytes)
-    // {"0":"1210","1":"15464","2":"3600","3":"8212","4":"14506"}
-    if (http_out_get("http://nnnn/nnnn.values", false, PBLINDEX_VALUES_COOKIE, &body) != HTTP_OK ||
-        http_out_send() != HTTP_OK) {
-        set_display_fail("values..");
-    }
-}
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
 
-void failed(int32_t cookie, int http_status, void *ctx) {
-    if (cookie == 0 ||
-        cookie == PBLINDEX_NAMES_COOKIE ||
-        cookie == PBLINDEX_VALUES_COOKIE) {
-        set_display_fail("failed()");
-    }
-}
-
-void success(int32_t cookie, int http_status, DictionaryIterator *dict, void *ctx) {
-    if (cookie != PBLINDEX_NAMES_COOKIE && cookie != PBLINDEX_VALUES_COOKIE)
+    if (!iter) 
         return;
-    int li = cookie == PBLINDEX_NAMES_COOKIE ? 0 : 1;
+
+    dict_write_tuplet(iter, &list_tuple);
+    dict_write_end(iter);
+
+    app_message_outbox_send();
+}
+
+static void in_received_handler(DictionaryIterator *iter, void *context) {
+    Tuple *names_tuple = dict_find(iter, QUOTE_KEY_NAMES);
+    Tuple *values_tuple = dict_find(iter, QUOTE_KEY_VALUES);
+    int li = -1;
+
+    if (names_tuple) 
+        li = 0;
+    if (values_tuple) 
+        li = 1;
+    if (li < 0)
+        return;
     for (int i=0; i<NUM_LINES; i++) {
-        Tuple *value = dict_find(dict, i);
+        Tuple *value = dict_find(iter, i);
         if (value) {
             static char str[2][NUM_LINES][10];
             strcpy(str[li][i], value->value->cstring);
@@ -90,22 +80,32 @@ void success(int32_t cookie, int http_status, DictionaryIterator *dict, void *ct
             text_layer_set_text(textLayer[li][i], "-");
         }
     }
-    if (!li) {
-        request_values();
-    } else {
+    if (!li) 
+        request_list(QUOTE_KEY_VALUES);
+    else 
         light_enable_interaction();
-	}
 }
 
-void reconnect(void *ctx) {
-    request_names();
+static void in_dropped_handler(AppMessageResult reason, void *context) {
+    set_display_fail("(dropped)");
+}
+
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+    set_display_fail("(send)");
+}
+
+static void app_message_init() {
+    app_message_register_inbox_received(in_received_handler);
+    app_message_register_inbox_dropped(in_dropped_handler);
+    app_message_register_outbox_failed(out_failed_handler);
+    app_message_open(124, 256);
 }
 
 void handle_init() {
-	window = window_create();
+    window = window_create();
     window_set_background_color(window, GColorBlack);
     window_stack_push(window, true);
-    
+
     for (int i=0; i<NUM_LINES; i++) {
         textLayer[0][i] = text_layer_create(GRect(5, 7+i*30, 144-5-COLUMN2_WIDTH, 30));
         textLayer[1][i] = text_layer_create(GRect(144-COLUMN2_WIDTH, 7+i*30, COLUMN2_WIDTH, 30));
@@ -117,42 +117,32 @@ void handle_init() {
         text_layer_set_text_color(textLayer[1][i], GColorWhite);
         text_layer_set_text_alignment(textLayer[1][i], GTextAlignmentRight);
     }
-    
+
     text_layer_set_text(textLayer[0][0], "pbl-index");
     text_layer_set_text(textLayer[0][1], "by epatel");
     text_layer_set_text(textLayer[0][3], "loading...");
-    
+
     for (int i=0; i<NUM_LINES; i++) {
-    	layer_add_child(window_get_root_layer(window), text_layer_get_layer(textLayer[0][i]));
-    	layer_add_child(window_get_root_layer(window), text_layer_get_layer(textLayer[1][i]));
+        layer_add_child(window_get_root_layer(window), text_layer_get_layer(textLayer[0][i]));
+        layer_add_child(window_get_root_layer(window), text_layer_get_layer(textLayer[1][i]));
     }
-    
-	app_message_open(124, 256);
 
-    http_capture_set_window(window);
-    
-    http_set_app_id(PBLINDEX_NAMES_COOKIE);
-    
-    http_register_callbacks((HTTPCallbacks){
-        .failure = failed,
-        .success = success,
-        .reconnect = reconnect,
-    }, NULL);
+    app_message_init();
 
-	request_names();
+    request_list(QUOTE_KEY_NAMES);
 }
 
 void handle_deinit() {
-    http_capture_deinit();
-	for (int i=0; i<NUM_LINES; i++) {
-		text_layer_destroy(textLayer[0][i]);
-		text_layer_destroy(textLayer[1][i]);
-	}
-	window_destroy(window);
+    for (int i=0; i<NUM_LINES; i++) {
+        text_layer_destroy(textLayer[0][i]);
+        text_layer_destroy(textLayer[1][i]);
+    }
+    window_destroy(window);
 }
 
 int main() {
-	handle_init();
+    handle_init();
     app_event_loop();
     handle_deinit();
+    return 0;
 }
